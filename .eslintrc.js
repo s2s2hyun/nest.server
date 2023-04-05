40e780bc0db382444238d908cfc4c0c028b42492
s2s2hyun <s2s2hyun0601@gmail.com>
2023-04-06 01:48:29 +0900

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,9 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     'prettier/prettier': [
       'error',
-      {},
+      {
+        endOfLine: 'auto',
+      },
       {
         fileInfoOptions: {
           withNodeModules: true,
