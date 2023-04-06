e7b35956c2592f1bcba33ca7b55b34b31ee2dedb
s2s2hyun <s2s2hyun0601@gmail.com>
2023-04-06 18:18:17 +0900

    CrD U 는 내일

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,69 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './boards.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
-  @Get()
-  getAllBoard() {
+  @Get('/')
+  getAllBoard(): Board[] {
     return this.boardsService.getAllBoards();
+  }
+
+  @Post()
+  async createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
+    try {
+      const createdBoard = this.boardsService.createBoard(createBoardDto);
+      return createdBoard;
+    } catch (err) {
+      if (err instanceof HttpException) {
+        throw err;
+      } else {
+        throw new HttpException(
+          'Internal Server Error',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
+  }
+
+  @Get('/:id')
+  async getBoardById(@Param('id') id: string): Promise<Board> {
+    try {
+      const board = await this.boardsService.getBoardById(id);
+      if (!board) {
+        throw new HttpException('Board not found', HttpStatus.NOT_FOUND);
+      }
+      return board;
+    } catch (err) {
+      if (err instanceof HttpException) {
+        throw err;
+      } else {
+        throw new HttpException(
+          'Internal Server Error',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
+  }
+
+  // getBoardById(@Param() params: string[]): Board {
+  //   return this.boardsService.getBoardById(id);
+  // }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,12 +1,36 @@
+import { CreateBoardDto } from './dto/create-board.dto';
 import { Injectable } from '@nestjs/common';
-
+import { Board, BoardStatus } from './boards.model';
+import { v1 as uuid } from 'uuid';
 @Injectable()
 export class BoardsService {
   //private 를 사용한 이유는 여기서 private 를 사용하지 않으면 다른 컴포넌트에서 이 board라는 배열을 변화를 줄수있기에 private 를 한것이다.
-  private boards = [];
+  private boards: Board[] = [];
 
   //위에 있는 boards 배열을 불러오는 함수
-  getAllBoards() {
+  getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    console.log(board); // Add this line to debug
+    return board;
+  }
+
+  getBoardById(id: string): Board {
+    return this.boards.find((board) => board.id === id);
+  }
+
+  deleteBoard(id: string): void {
+    this.boards = this.boards.filter((board) => board.id !== id);
   }
 }
