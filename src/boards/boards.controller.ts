3d75c2c4714912a3056e2ebf87cffbee63be38cc
s2s2hyun <s2s2hyun0601@gmail.com>
2023-05-23 19:10:21 +0900

    test-1

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -10,12 +10,18 @@ import {
   Patch,
   Put,
   NotFoundException,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { BoardType, BoardStatus } from './boards.model';
 import { CreateBoardDto } from './dtos/create-board.dto';
 import { UpdateBoardDto } from './dtos/update-board.dto';
 import { Board } from './board.entity';
+import { v4 as uuid } from 'uuid';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as fsPromises from 'fs/promises';
+import * as path from 'path';
 
 @Controller('boards')
 export class BoardsController {
@@ -32,15 +38,57 @@ export class BoardsController {
     }));
   }
 
-  @Post()
+  // @Post('/')
+  // async createBoard(
+  //   @Body() createBoardDto: CreateBoardDto,
+  // ): Promise<BoardType> {
+  //   try {
+  //     const createdBoard = await this.boardsService.createBoard(createBoardDto);
+  //     console.log(createdBoard);
+  //     return createdBoard;
+  //   } catch (err) {
+  //     if (err instanceof HttpException) {
+  //       throw err;
+  //     } else {
+  //       throw new HttpException(
+  //         'Internal Server Error',
+  //         HttpStatus.INTERNAL_SERVER_ERROR,
+  //       );
+  //     }
+  //   }
+  // }
+  @Post('/')
+  @UseInterceptors(FileInterceptor('image'))
   async createBoard(
     @Body() createBoardDto: CreateBoardDto,
+    @UploadedFile() image: Express.Multer.File,
   ): Promise<BoardType> {
     try {
-      const createdBoard = await this.boardsService.createBoard(createBoardDto);
-      console.log(createdBoard);
+      // Get the file name
+      const imageName = uuid() + path.extname(image.originalname);
+
+      // Move the uploaded image file to the images folder
+      const imagePath = path.join(
+        __dirname,
+        '..',
+        '..',
+        'public',
+        'images',
+        imageName,
+      );
+      console.log('Image Path:', imagePath); // Add this line to check the image path
+      await fsPromises.rename(image.path, imagePath);
+
+      // Create the board with the image path
+      const createdBoard = await this.boardsService.createBoard({
+        ...createBoardDto,
+        imagePath: `/images/${imageName}`,
+      });
+
+      console.log('Created Board:', createdBoard); // Add this line to check the created board
       return createdBoard;
     } catch (err) {
+      console.log('Error:', err); // Add this line to log the error
       if (err instanceof HttpException) {
         throw err;
       } else {
@@ -76,10 +124,6 @@ export class BoardsController {
       }
     }
   }
-
-  // getBoardById(@Param() params: string[]): Board {
-  //   return this.boardsService.getBoardById(id);
-  // }
 
   @Delete('/:id')
   deleteBoard(@Param('id') id: string): void {
--- src/boards/dtos/create-board.dto.ts
+++ src/boards/dtos/create-board.dto.ts
@@ -1,4 +1,5 @@
 export class CreateBoardDto {
   title: string;
   description: string;
+  imagePath: string;
 }
