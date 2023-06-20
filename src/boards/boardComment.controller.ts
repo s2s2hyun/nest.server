34e179395431d9485002e435df0897cf038b0038
s2s2hyun <s2s2hyun0601@gmail.com>
2023-06-20 23:44:37 +0900

    test 완료 comment post

--- src/boards/boardComment.controller.ts
+++ src/boards/boardComment.controller.ts
@@ -8,6 +8,7 @@ export class CommentsController {
   @Post()
   create(@Body() commentData, @Param('boardId') boardId: string) {
     // boardId를 사용하여 게시글을 찾고, 해당 게시글에 commentData를 추가합니다.
+    // 6-20 댓글 post 완료 확인 21일 Get 요청으로 id 값 기준으로 배열로 생성 예정
     return this.commentsService.create(boardId, commentData);
   }
 }
